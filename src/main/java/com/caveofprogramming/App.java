129a6c4499bc8091eb19b82444e0154f7e5a5f14
j4sysiak <j4sysiak@gmail.com>
2018-11-18 16:16:41 +0100

    20181118_1616

--- src/main/java/com/caveofprogramming/App.java
+++ src/main/java/com/caveofprogramming/App.java
@@ -1,23 +1,22 @@
 package com.caveofprogramming;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
-@Controller
-@EnableAutoConfiguration
-public class App {
+@SpringBootApplication
+public class App extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
 	
-	@RequestMapping("/")
-	@ResponseBody
-	String home() {
-		return "Hello World!";
-	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(App.class);
+    }
+	
+
 
 }
